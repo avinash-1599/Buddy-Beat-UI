e4ff45d574d3126c26524a6c8ff0bf4601da2730
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-02-08 11:48:12 +0530

    dropdown fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
 import { removeUser } from "../utils/userSlice";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const NavBar = () => {
     // subscribe to the user store using useSelector hook
@@ -11,10 +12,13 @@ const NavBar = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     const handleLogout = async () => {
         try {
             await axios.post(BASE_URL+'/logout', {}, {withCredentials: true});
             dispatch(removeUser());
+            setIsDropdownOpen(false);
             return navigate('/login');
         } catch(err) {
             console.log("Error logging out", err);
@@ -29,14 +33,14 @@ const NavBar = () => {
             </div>
             <div className="flex-none gap-2">
                 {user && (
-                    <div className="dropdown dropdown-end mx-5 flex">
+                    <div onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="dropdown dropdown-end mx-5 flex">
                         <p className="px-4 text-sm text-white mt-2">Welcome, {user.firstName}</p>
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <img alt="user photo" src={user.photoUrl} />
                             </div>
                         </div>
-                        <ul
+                        {isDropdownOpen && (<ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-gray-800 text-white rounded-box z-[1] mt-3 w-52 p-2 shadow">
                             <li>
@@ -45,11 +49,11 @@ const NavBar = () => {
                                     <span className="badge">New</span>
                                 </Link>
                             </li>
-                            <li><Link to='/connections'>Connections</Link></li>
-                            <li><Link to='/requests'>Requests</Link></li>
-                            <li><Link to='/premium'>Premium</Link></li>
+                            <li onClick={() => setIsDropdownOpen(false)}><Link to='/connections'>Connections</Link></li>
+                            <li onClick={() => setIsDropdownOpen(false)}><Link to='/requests'>Requests</Link></li>
+                            <li onClick={() => setIsDropdownOpen(false)}><Link to='/premium'>Premium</Link></li>
                             <li><a onClick={() => handleLogout()}>Logout</a></li>
-                        </ul>
+                        </ul>)}
                     </div>
                 )}
             </div>
