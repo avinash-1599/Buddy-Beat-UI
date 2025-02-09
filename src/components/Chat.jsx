6fd2219ad044736d49d1ff8774a456992ffcf474
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-02-09 09:04:29 +0530

    changes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Send } from "lucide-react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
