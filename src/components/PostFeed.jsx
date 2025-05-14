7d90dcf7444e13f725d73bd70c26edc9635a8807
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-05-14 16:14:07 +0530

    changes

--- src/components/PostFeed.jsx
+++ src/components/PostFeed.jsx
@@ -56,47 +56,18 @@ const PostFeed = () => {
         fetchRecentPublicPosts();
     }, [user]);
 
-    // if (!posts?.length) {
-    //     return (
-    //         <div className="flex flex-col lg:flex-row my-10 w-full gap-4 px-2 sm:px-4">
-    //             {/* Left - Sidebar */}
-    //             <div className="hidden lg:block w-2/12">
-    //                 <SideBar />
-    //             </div>
-    
-    //             {/* Center - Main Content */}
-    //             <div className="w-full lg:w-7/12 overflow-y-auto mt-5 px-4">
-    //                 <div className="p-6 bg-gray-900 text-white rounded-lg shadow-lg">
-    //                     <div className="flex items-center justify-between border-b border-gray-700 pb-4 mb-6">
-    //                         <h2 className="text-3xl font-semibold text-white">📢 Latest Public Posts</h2>
-    //                         <button
-    //                             className="flex items-center gap-2 px-2 py-1 text-black bg-white rounded-lg shadow hover:bg-yellow-400 transition-all"
-    //                             onClick={() => navigate("/create-post")}
-    //                         >
-    //                             <img src="/create-post-icon.png" alt="Create post" className="h-6 w-6" />
-    //                             <span>Create Post</span>
-    //                         </button>
-    //                     </div>
-    
-    //                     <p className="mb-4 text-md text-gray-500">
-    //                      👋 Looks like you're just getting started. Follow users or create a post to kick off your experience. Here's what others are sharing publicly!"
-    //                     </p>
-    
-    //                     <div className="space-y-6 w-full max-w-3xl mx-auto overflow-hidden break-words">
-    //                         {recentPublicPosts.map((post) => (
-    //                             <Post key={post._id} post={post} />
-    //                         ))}
-    //                     </div>
-    //                 </div>
-    //             </div>
-    
-    //             {/* Right - Friend Recommendations */}
-    //             <div className="hidden lg:block w-3/12">
-    //                 <FriendRecommendations />
-    //             </div>
-    //         </div>
-    //     );
-    // }    
+    useEffect(() => {
+        if (isSidebarOpen) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = "auto";
+        }
+      
+        return () => {
+          document.body.style.overflow = "auto";
+        };
+      }, [isSidebarOpen]);
+      
 
     if (!posts?.length) {
         return (
@@ -105,7 +76,7 @@ const PostFeed = () => {
                 <div className="lg:hidden fixed top-20 left-2 z-50">
                     <button
                         onClick={() => setIsSidebarOpen(prev => !prev)}
-                        className="bg-white text-black p-2 rounded-full shadow-md"
+                        className="bg-gray-600 text-black p-3 rounded-full shadow-md"
                     >
                         <img src="/menu-icon.png" alt="Menu" className="w-4 h-4" />
                     </button>
@@ -114,7 +85,7 @@ const PostFeed = () => {
                 {/* Mobile Sidebar Overlay */}
                 {isSidebarOpen && (
                 <div className="lg:hidden fixed inset-0 bg-black bg-opacity-60 z-40 flex top-8">
-                    <div className="w-1/2 max-w-[200px] h-full relative z-30">
+                    <div className="w-1/2 max-w-[200px] h-full relative z-30 mt-10">
                         <SideBar onClose={() => setIsSidebarOpen(false)} />
                     </div>
                     <div className="flex-1" onClick={() => setIsSidebarOpen(false)} />
@@ -131,7 +102,7 @@ const PostFeed = () => {
                     <div className="w-full lg:w-7/12 overflow-y-auto mt-5 px-4">
                         <div className="p-6 bg-gray-900 text-white rounded-lg shadow-lg">
                             <div className="flex items-center justify-between border-b border-gray-700 pb-4 mb-6">
-                                <h2 className="text-3xl font-semibold text-white">📢 Latest Public Posts</h2>
+                                <h2 className="text-3xl font-semibold text-white ml-10">📢 Latest Public Posts</h2>
                                 <button
                                     className="flex items-center gap-2 px-2 py-1 text-black bg-white rounded-lg shadow hover:bg-yellow-400 transition-all"
                                     onClick={() => navigate("/create-post")}
@@ -166,24 +137,24 @@ const PostFeed = () => {
     return (
         <>
             {/* Mobile-only toggle icon */}
-        <div className="lg:hidden fixed top-20 left-2 z-50">
-        <button
-            onClick={() => setIsSidebarOpen(prev => !prev)}
-            className="bg-white text-black p-2 rounded-full shadow-md"
-            >
-            <img src="/menu-icon.png" alt="Menu" className="w-4 h-4" />
-        </button>
-        </div>
-
-        {/* Mobile Sidebar Overlay */}
-        {isSidebarOpen && (
-            <div className="lg:hidden fixed inset-0 bg-black bg-opacity-60 z-40 flex top-8">
-                <div className="w-1/2 max-w-[200px] h-full relative z-30">
-                    <SideBar onClose={() => setIsSidebarOpen(false)} />
+            <div className="lg:hidden fixed top-20 left-2 z-50">
+                    <button
+                        onClick={() => setIsSidebarOpen(prev => !prev)}
+                        className="bg-gray-600 text-black p-3 rounded-full shadow-md"
+                    >
+                        <img src="/menu-icon.png" alt="Menu" className="w-4 h-4" />
+                    </button>
                 </div>
-                <div className="flex-1" onClick={() => setIsSidebarOpen(false)} />
-            </div>
-        )}
+    
+                {/* Mobile Sidebar Overlay */}
+                {isSidebarOpen && (
+                <div className="lg:hidden fixed inset-0 bg-black bg-opacity-60 z-40 flex top-8">
+                    <div className="w-1/2 max-w-[200px] h-full relative z-30 mt-10 overflow-y-auto">
+                        <SideBar onClose={() => setIsSidebarOpen(false)} />
+                    </div>
+                    <div className="flex-1" onClick={() => setIsSidebarOpen(false)} />
+                </div>
+                )}
 
         {/* Main Content */}
         <div className="flex flex-col lg:flex-row my-10 w-full gap-4 px-2 sm:px-4">
@@ -197,7 +168,7 @@ const PostFeed = () => {
             <div className="w-full lg:w-7/12 order-1 lg:order-2">
                 <div className="p-6 bg-gray-900 text-white rounded-lg shadow-lg">
                     <div className="flex items-center justify-between border-b border-gray-700 pb-4 mb-6 relative">
-                        <h2 className="text-2xl font-extrabold text-white">📢 Latest Posts</h2>
+                        <h2 className="text-2xl font-extrabold text-white ml-5">📢 Latest Posts</h2>
                         <div
                             className="relative flex items-center"
                             onMouseEnter={() => setShowTooltip(true)}
